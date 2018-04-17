9eb0b7ffe5db90b5bebda2783af631156c1e5e34
Andreas Philippi <anphil@users.noreply.github.com>
2018-04-17 15:20:08 +0200

    Generate note model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_17_124251) do
+ActiveRecord::Schema.define(version: 2018_04_17_131438) do
+
+  create_table "notes", force: :cascade do |t|
+    t.string "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
