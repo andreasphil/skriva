4c12aac08ffed57bb4a57e8f60b96f1e40956254
Andreas Philippi <anphil@users.noreply.github.com>
2018-04-17 17:01:10 +0200

    Add basic note controller

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,6 @@
+class NotesController < ApplicationController
+  before_action :require_login
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get 'home/index'
+
+  resources :notes, only: :index
+
   root 'home#index'
 end
