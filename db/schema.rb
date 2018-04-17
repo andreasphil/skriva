8af8bba54052afb4b90b6c969dd3c967ccb22aca
Andreas Philippi <anphil@users.noreply.github.com>
2018-04-17 16:14:16 +0200

    Add user reference to note model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_17_133632) do
+ActiveRecord::Schema.define(version: 2018_04_17_140229) do
 
   create_table "notes", force: :cascade do |t|
     t.string "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_notes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
