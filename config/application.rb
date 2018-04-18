5995454cb450af91f2d10ab38bb5b4f577c663a9
Andreas Philippi <anphil@users.noreply.github.com>
2018-04-18 16:14:48 +0200

    Implement styles for login and signup forms

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,13 @@ module Skriva
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # Set layouts for clearance
+    config.to_prepare do
+      Clearance::PasswordsController.layout 'panel'
+      Clearance::SessionsController.layout 'panel'
+      Clearance::UsersController.layout 'panel'
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,5 +1,7 @@
 Clearance.configure do |config|
+  # TODO: Replace address
   config.mailer_sender = "reply@example.com"
-  config.rotate_csrf_on_sign_in = true
+
   config.redirect_url = '/notes/index'
+  config.rotate_csrf_on_sign_in = true
 end
