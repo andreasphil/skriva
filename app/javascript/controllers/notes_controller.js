0d4c1eb7d5160c502bc0ef245ea8ea5dd80e985c
Andreas Philippi <anphil@users.noreply.github.com>
2018-05-20 15:15:56 +0200

    Add user feedback to note saving button

--- app/javascript/controllers/notes_controller.js
+++ app/javascript/controllers/notes_controller.js
@@ -1,17 +1,74 @@
 import { Controller } from 'stimulus'
 
-export default class extends Controller
-{
-  static targets = ['note', 'input', 'submit']
+export default class extends Controller {
+  static targets = ['note', 'saveButton', 'input', 'submitButton']
 
-  save()
-  {
+  save() {
     const noteContent = this.noteTarget.innerHTML
     const shadowNoteInput = this.inputTarget
-    const shadowSubmit = this.submitTarget
+    const shadowSubmitButton = this.submitButtonTarget
 
     // Copy content from contenteditable field to form and submit
     shadowNoteInput.value = noteContent
-    shadowSubmit.click()
+    shadowSubmitButton.click()
+  }
+
+  onNoteInput(e) {
+    // Activate the button to save the note when it has changed
+    this.setSaveButtonState('unsavedChanges')
+  }
+
+  onSend(e) {
+    // Indicate that the note is being saved
+    this.setSaveButtonState('saving')
+  }
+
+  onSuccess(e) {
+    // Indicate that the note was successfully saved
+    this.setSaveButtonState('success')
+
+    // Go back to default state
+    setTimeout(() =>
+    {
+      this.setSaveButtonState('saved')
+    }, 1000)
+  }
+
+  onError(e) {
+    // Indicate that the saving failed
+    this.setSaveButtonState('error')
+  }
+
+  setSaveButtonState(state) {
+    const allClasses = ['disabled', 'loading', 'btn-success', 'btn-error']
+
+    // TODO: i18n -> Use strings from Rails
+    const states = {
+      unsavedChanges: {
+        classes: ['test'],
+        message: 'Save'
+      },
+      saving: {
+        classes: ['loading'],
+        message: 'Saving ...'
+      },
+      success: {
+        classes: ['disabled', 'btn-success'],
+        message: 'Saved'
+      },
+      saved: {
+        classes: ['disabled'],
+        message: 'Saved'
+      },
+      error: {
+        classes: ['btn-error'],
+        message: 'Try again'
+       }
+    }
+
+    const saveButton = this.saveButtonTarget
+    saveButton.classList.remove(...allClasses)
+    saveButton.classList.add(...states[state].classes)
+    saveButton.innerHTML = states[state].message
   }
 }
