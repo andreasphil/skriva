64fafc2abdded02e89d9dc992e8ca273c182b0fd
Andreas Philippi <anphil@users.noreply.github.com>
2018-05-29 19:01:34 +0200

    Add helper to mark links to the current page with an "active" class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@
 
 # Application helpers
 module ApplicationHelper
+  # Create a link that will have the 'active' class set if its url matches the
+  # current page
+  def navbar_link_to(body, url, html_options)
+    html_options = {} if html_options.blank?
+    html_options[:class] = {} if html_options[:class].blank?
+
+    html_options[:class] << 'active' if current_page?(url)
+
+    link_to body, url, html_options
+  end
 end
