e5180ef5b7f66c36f35cc38c77e43f7016f14702
Andreas Philippi <anphil@users.noreply.github.com>
2018-12-15 18:05:38 +0100

    Implement navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+# frozen_string_literal: false
 
 # Application helpers
 module ApplicationHelper
@@ -8,7 +8,9 @@ module ApplicationHelper
     html_options = {} if html_options.blank?
     html_options[:class] = {} if html_options[:class].blank?
 
-    html_options[:class] << 'active' if current_page?(url)
+    active_class_name = 'active'
+    active_class_name.prepend(' ') if html_options[:class].is_a?(String)
+    html_options[:class] << active_class_name if current_page?(url)
 
     link_to body, url, html_options
   end
