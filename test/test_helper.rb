db451da3a733c129fdda3557904c25feb36ca7b3
Andreas Philippi <anphil@users.noreply.github.com>
2018-12-16 15:11:25 +0100

    Fix Rubocop offense

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,8 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
-class ActiveSupport
+module ActiveSupport
+  # TODO
   class TestCase
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical
     # order.
